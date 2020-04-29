8ef886439d51bf459168bad8668fe1f316765423
Runeet Vashisht <runeetvashisht@hotmail.com>
2020-04-29 19:05:49 -0400

    Added Tag helpers and fixed menu service.

--- WiredBrainCoffee/Pages/Item.cshtml.cs
+++ WiredBrainCoffee/Pages/Item.cshtml.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffee.Models;
+using WiredBrainCoffee.Services;
 
 namespace WiredBrainCoffee.Pages
 {
     public class ItemModel : PageModel
-    {
-        public string Message { get; set; }
+    {        
 
+        public MenuItem Item { get; private set; }
         public void OnGet(int id)
         {
-            Message = "The id is " + id;
+            var menuService = new MenuService();
+            Item = menuService.GetMenuItems().FirstOrDefault(x => x.Id == id);
+
         }
     }
 }
