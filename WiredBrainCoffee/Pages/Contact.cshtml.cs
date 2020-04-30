47d802dca4d2ec8326541cef22eef9636e70a2ed
Runeet Vashisht <runeetvashisht@hotmail.com>
2020-04-30 13:59:53 -0400

    Added form validation.

--- WiredBrainCoffee/Pages/Contact.cshtml.cs
+++ WiredBrainCoffee/Pages/Contact.cshtml.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+using WiredBrainCoffee.Models;
+using WiredBrainCoffee.Services;
 
 namespace WiredBrainCoffee.Pages
 {
     public class ContactModel : PageModel
     {
-        public string Message { get; set; }
+        [BindProperty]
+        public Contact  Contact { get; set; }
+        public string Message { get; private set; }
 
         public void OnGet()
         {
-            Message = "Your contact page.";
+            
         }
+
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                EmailService.SendEmail(Contact);
+                Message = "Your message has been sent.";
+            }
+        }
+
     }
 }
---
+++ WiredBrainCoffee/Services/EmailService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.KeyVault.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Model = WiredBrainCoffee.Models;
+
+namespace WiredBrainCoffee.Services
+{
+    public class EmailService 
+    {
+        public static void SendEmail(Models.Contact contact)
+        {
+            //todo
+        }
+
+        public static void SendEmail(string contact)
+        {
+            //todo
+        }
+
+    }
+}
